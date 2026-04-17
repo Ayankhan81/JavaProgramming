00f1796ac8160d15b742a2f93565df107123ebd5
Ayan khan <mdayankhan166@gmail.com>
2026-04-17 23:01:02 +0530

    Prime Number

--- Loop/whileLoop/p18.java
+++ Loop/whileLoop/p18.java
@@ -3,7 +3,22 @@
 package whileLoop;
 
 public class p18 {
-    public static void main (String[] args){
-        int i=
+    public static void main(String[] args) {
+       int n=1;
+       while (n<=10) {
+        int i=1;
+        int count=0;
+
+            while (i<=n) {
+                if(n%i==0){
+                    count++;
+                }
+                i++;
+            }
+        if(count==2){
+            System.out.println(n+" ");
+        }
+        n++;
+       }
     }
 }
